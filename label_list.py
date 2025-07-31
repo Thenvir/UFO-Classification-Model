f65a6ff2ad6741f11deafc5d43705c72c3e42224
Thenvir <thenvira@gmail.com>
2025-07-31 11:35:55 -0400

    update default label list

--- label_list.py
+++ label_list.py
@@ -1,1 +1,12 @@
-labels = ["a photo of a UFO", "a photo of a grey hat", "a photo of a ball", "a photo of a cat", "a photo of a dog"]
+labels = [
+    "a photo of a UFO",
+    "a photo of a grey hat",
+    "a photo of a ball",
+    "a photo of a cat",
+    "a photo of a dog",
+    "a photo of a flying saucer",
+    "a photo of an alien spacecraft",
+    "a photo of a mysterious light in the sky",
+    "a photo of a crop circle",
+    "a photo of an alien being"
+]
